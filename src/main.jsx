cb6ff76975277b6a41566d4a9fbd0279b9d20527
Siddharth <siddharthnmishra7@gmail.com>
2024-12-08 22:10:13 +0530

    feat : add playground route

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router';
+import Playground from './Playground.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/playground" element={<Playground />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
