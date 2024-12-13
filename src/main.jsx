6b7cfc72bd22a29892f59a6a62344f85d373758b
shriyaaditya <shriyadalai6@gmail.com>
2024-12-13 07:59:59 +0530

    rendered navbar hero and footer in playground file

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,12 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Playground from './Playground.jsx';
-import Hero from './components/hero.jsx';
-import Navbar from './components/navbar.jsx';
-import Footer from './components/footer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/navbar" element={<Navbar />} />
-        <Route path="/hero" element={<Hero />} />
-        <Route path="/footer" element={<Footer />} />
         <Route path="/playground" element={<Playground />} />
       </Routes>
     </BrowserRouter>
